0365804414b063b72c2dac01ce1434b310c9908f
Iris McDonald <S00507769@acad.tri-c.edu>
2018-02-21 23:04:48 -0500

    Basic logic to load car list functional.  Ready to add functions to interface.

--- FleetSim.Console/Program.cs
+++ FleetSim.Console/Program.cs
@@ -3,13 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FleetSim.Objects;
 
-namespace FleetSim.Console
+namespace FleetSim
 {
     class Program
     {
+        static void LoadCars()
+        {
+            var theCars = new List<Car>
+                {
+                new Car()
+                {
+                    VIN = "12345",
+                    Color = "white",
+                    LastServicedMileage = 0,
+                    Mileage = 0,
+                    Model = "Mazda CX5",
+                    ModelYear = 2016
+                },
+                new Car()
+                {
+                    VIN = "67890",
+                    Color = "red",
+                    LastServicedMileage = 0,
+                    Mileage = 0,
+                    Model = "Honda CRV",
+                    ModelYear = 2014
+                },
+                new Car()
+                {
+                    VIN = "ABCDE",
+                    Color = "blue",
+                    LastServicedMileage = 0,
+                    Mileage = 0,
+                    Model = "Jeep Wrangler",
+                    ModelYear = 2010
+                },
+                new Car()
+                {
+                    VIN = "ABCDE",
+                    Color = "red",
+                    LastServicedMileage = 0,
+                    Mileage = 0,
+                    Model = "Alpha Romeo Stelvio",
+                    ModelYear = 2018
+                },
+                new Car()
+                {
+                    VIN = "ABCDE",
+                    Color = "white",
+                    LastServicedMileage = 0,
+                    Mileage = 0,
+                    Model = "Ford F150",
+                    ModelYear = 2004
+                },
+            };
+            foreach (Car theCar in theCars)
+            {
+                System.Console.WriteLine(theCar.Model + " " + theCar.ModelYear);
+            }
+                /*Console.Readkey();*/
+        }
+    
+       
         static void Main(string[] args)
         {
+            LoadCars();
         }
     }
 }
